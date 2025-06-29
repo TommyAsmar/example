4c90fe0aea1617955e3e85a372877a790b2f3b73
Tommy El Asmar <tommyelasmar@gmail.com>
2025-06-30 02:24:45 +0300

    Relationships

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Employer;
 
 class Job extends Model
 {
@@ -11,4 +11,14 @@ class Job extends Model
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'job_tag', 'job_listing_id', 'tag_id');
+    }
 }
